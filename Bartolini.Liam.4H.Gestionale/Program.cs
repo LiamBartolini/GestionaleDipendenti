decf33718d28650cffd130d02bb6382f89878dea
unknown <liambartolini@gmail.com>
2020-09-20 00:26:28 +0200

    Aggiunta lettura da file per i vettori

--- Bartolini.Liam.4H.Gestionale/Program.cs
+++ Bartolini.Liam.4H.Gestionale/Program.cs
@@ -7,80 +7,176 @@ namespace Bartolini.Liam._4H.Gestionale
     {
         static void Main(string[] args)
         {
-            string[] cognomi = new string[] { "bartolini", "bronzetti", "lu", "amantini", "tiraferri" };
-            string[] reparto = new string[] {"amm", "amm", "magaz", "amm", "sped"};
-            int[] ore = new int[] {2, 6, 3, 7, 9};
-
-            Console.WriteLine("Scegli quale modalità di ricerca usare: \r\n1. Per cognome\r\n2. Per reparto");
-            string ricerca = Console.ReadLine();
-
-            if(ricerca == "1")
+            string path = @"C:/Users/Desktop/Desktop/Bartolini.Liam.4H.Gestionale/Bartolini.Liam.4H.Gestionale/dati.txt";
+            if (File.Exists(path))
             {
-                bool flag = false;
-
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Hai scelto la prima opzione!");
+                Console.WriteLine("Apro il file dati.txt...");
                 Console.ResetColor();
 
-                Console.WriteLine("Inserisci il cognome da cercare: ");
-                string cognome = Console.ReadLine();
-                string cognome_Lower = cognome.ToLower();
+                StreamReader fin = new StreamReader(path);
 
-                for (int i = 0; i < cognomi.Length; i++)
+                string riga = fin.ReadLine();
+                string[] cognomi_Stream = riga.Split(',');
+                riga = fin.ReadLine();
+                string[] reparto_Stream = riga.Split(',');
+                riga = fin.ReadLine();
+                string[] ore_Stream = riga.Split(',');
+
+                fin.Close();
+
+                Console.WriteLine("Scegli quale modalità di ricerca usare: \r\n1. Per cognome\r\n2. Per reparto");
+                string ricercaStream = Console.ReadLine();
+
+                if(ricercaStream == "1")
                 {
-                    string cognomi_Lower = cognomi[i].ToLower();
+                    bool flag = false;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Hai scelto la prima opzione!");
+                    Console.ResetColor();
 
-                    if (cognome_Lower == cognomi_Lower)
+                    Console.WriteLine("Inserisci il cognome da cercare: ");
+                    string cognome = Console.ReadLine();
+                    string cognome_Lower = cognome.ToLower();
+
+                    for (int i = 0; i < cognomi_Stream.Length; i++)
                     {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine($"il dipendente {cognomi[i]} ha fatto {ore[i]} ore di straordinario");
-                        flag = true;
+                        string cognomi_Lower = cognomi_Stream[i].ToLower();
+
+                        if (cognome_Lower == cognomi_Lower)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"il dipendente {cognomi_Stream[i]} ha fatto {ore_Stream[i]} ore di straordinario");
+                            flag = true;
+                        }
+                    }
+
+                    Console.ResetColor();
+
+                    if (!flag)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Non è stato trovato nessun dipendente...");
+                        Console.ResetColor();
                     }
                 }
-
-                Console.ResetColor();
-                
-                if (!flag)
+                else
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Non è stato trovato nessun dipendente...");
+                    int oreStraodinario = 0;
+                    bool flag = false;
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Hai scelto la seconda opzione!");
                     Console.ResetColor();
+
+                    Console.WriteLine("Inserisci il reparto da cercare: ");
+                    string reparto_Ricerca = Console.ReadLine();
+                    string cognome_Lower = reparto_Ricerca.ToLower();
+
+                    for (int i = 0; i < reparto_Stream.Length; i++)
+                    {
+                        string reparto_Lower = reparto_Stream[i].ToLower();
+                        if (cognome_Lower == reparto_Lower)
+                        {
+                            oreStraodinario += Convert.ToInt32(ore_Stream[i]);
+                            flag = true;
+                        }
+                    }
+
+                    if (!flag)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Non è stato trovato nessun reparto..");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"I dipendenti del reparto '{reparto_Ricerca}' hanno fatto {oreStraodinario} ore di straodinario complessive");
+                        Console.ResetColor();
+                    }
                 }
             }
             else
             {
-                int oreStraodinario = 0;
-                bool flag = false;
-
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Hai scelto la seconda opzione!");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Il file non è disponibile...");
                 Console.ResetColor();
 
-                Console.WriteLine("Inserisci il reparto da cercare: ");
-                string reparto_Ricerca = Console.ReadLine();
-                string cognome_Lower = reparto_Ricerca.ToLower();
-                
-                for(int i = 0; i < reparto.Length; i++)
-                {
-                    string reparto_Lower = reparto[i].ToLower();
-                    if (cognome_Lower == reparto_Lower)
-                    {
-                        oreStraodinario += ore[i];
-                        flag = true;
-                    }
-                }
+                string[] cognomi = new string[] { "bartolini", "bronzetti", "lu", "amantini", "tiraferri" };
+                string[] reparto = new string[] { "amm", "amm", "magaz", "amm", "sped" };
+                int[] ore = new int[] { 2, 6, 3, 7, 9 };
 
-                if(!flag)
+                Console.WriteLine("Scegli quale modalità di ricerca usare: \r\n1. Per cognome\r\n2. Per reparto");
+                string ricerca = Console.ReadLine();
+
+                if (ricerca == "1")
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Non è stato trovato nessun reparto..");
+                    bool flag = false;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Hai scelto la prima opzione!");
                     Console.ResetColor();
+
+                    Console.WriteLine("Inserisci il cognome da cercare: ");
+                    string cognome = Console.ReadLine();
+                    string cognome_Lower = cognome.ToLower();
+
+                    for (int i = 0; i < cognomi.Length; i++)
+                    {
+                        string cognomi_Lower = cognomi[i].ToLower();
+
+                        if (cognome_Lower == cognomi_Lower)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"il dipendente {cognomi[i]} ha fatto {ore[i]} ore di straordinario");
+                            flag = true;
+                        }
+                    }
+
+                    Console.ResetColor();
+
+                    if (!flag)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Non è stato trovato nessun dipendente...");
+                        Console.ResetColor();
+                    }
                 }
                 else
                 {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine($"I dipendenti del reparto '{reparto_Ricerca}' hanno fatto {oreStraodinario} ore di straodinario complessive");
+                    int oreStraodinario = 0;
+                    bool flag = false;
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Hai scelto la seconda opzione!");
                     Console.ResetColor();
+
+                    Console.WriteLine("Inserisci il reparto da cercare: ");
+                    string reparto_Ricerca = Console.ReadLine();
+                    string cognome_Lower = reparto_Ricerca.ToLower();
+
+                    for (int i = 0; i < reparto.Length; i++)
+                    {
+                        string reparto_Lower = reparto[i].ToLower();
+                        if (cognome_Lower == reparto_Lower)
+                        {
+                            oreStraodinario += ore[i];
+                            flag = true;
+                        }
+                    }
+
+                    if (!flag)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Non è stato trovato nessun reparto..");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"I dipendenti del reparto '{reparto_Ricerca}' hanno fatto {oreStraodinario} ore di straodinario complessive");
+                        Console.ResetColor();
+                    }
                 }
             }
         }
